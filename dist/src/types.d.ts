8480a65b9d8bf5b0374e1acd7cf228e1f27c29ce
Dmitry Yakimov <welldan97@gmail.com>
2018-11-22 18:40:13 +0000

    More universal UserProfile

--- dist/src/types.d.ts
+++ dist/src/types.d.ts
@@ -4,14 +4,12 @@ import { Request } from '@loopback/rest';
  * and returns an authenticated user
  */
 export interface AuthenticateFn {
-    (request: Request): Promise<UserProfile | undefined>;
+  (request: Request): Promise<UserProfile | undefined>;
 }
 /**
  * interface definition of a user profile
  * http://openid.net/specs/openid-connect-core-1_0.html#StandardClaims
  */
 export interface UserProfile {
-    id: string;
-    name?: string;
-    email?: string;
+  [key: string]: any;
 }
